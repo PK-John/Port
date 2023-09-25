467cad6ae04c9ad9a68e13fd75cf056555afc053
João Victor <jonh_victor@outlook.com>
2023-09-25 14:14:18 -0300

    Alterações dos documentos que estavam na página

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var typed = new Typed(".multiplo-texto", { 
+    strings : ["Estudante de Programação", "Desenvolvedor Web"] ,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+var logo = window.document.getElementById('logo')
+var sobre = window.document.getElementById('sobre')
+
+logo.addEventListener('click', refresh)
+sobre.addEventListener('click', descricao)
+
+function refresh(){
+    window.location.reload()
+}
+
+function descricao(){
+    document.body.innerHTML = document.getElementById("body2").innerHTML;
+}
